1163799956658d7895989354d2c807518a971464
suraj bisht <53903434+sub0101@users.noreply.github.com>
2024-05-15 19:51:36 +0530

    Update settings.py

--- minor_project/settings.py
+++ minor_project/settings.py
@@ -156,7 +156,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, “staticfiles_build”, “static”)
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR , 'static'),
