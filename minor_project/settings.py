3f1d89f9469678f85be3297b725d36128aa0b3bd
suraj bisht <53903434+sub0101@users.noreply.github.com>
2024-05-15 19:26:00 +0530

    Update settings.py

--- minor_project/settings.py
+++ minor_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1=7rzfq45=xgf_s%2filf(f2$wma1rwb%fm41(&q!elzf@ibf0'
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', ".vercel.app",'localhost']
 AUTH_USER_MODEL = 'vkeel.User'
